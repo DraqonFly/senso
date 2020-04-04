51067d27a739181ae791a274b875e09ec7e91cbe
DraqonFly <DraqonFly@github.com>
2020-04-05 01:49:14 +0200

    Loop AI-Player turns until gameover, upscaling lights and turns count

--- src/js/classes/controller.js
+++ src/js/classes/controller.js
@@ -1,15 +1,41 @@
+import { Light } from "./light.js";
+
+
 class Controller
 {
+    hp;
     blinkOrder;
     blinkDuration;
-    blinkOrderLength = 5;
+    blinkOrderLength;
     lights;
-    record;
+    record = false;
     recordCount = 0;
 
     constructor(initialBlinkDuration)
     {
         this.blinkDuration = initialBlinkDuration;
+        this.hp = 3;
+        this.blinkOrderLength = 3;
+    }
+
+    addLight = () => {
+        console.log("adding another light");
+        let added = false;
+        [...document.querySelectorAll(".lightouter")].forEach ( elem => {
+            if(added === false){
+            switch(this.lights.length) {
+                case 3: added = true; this.lights.push(new Light("YellowLight", ["#D5B85A", "#FFF200"])); document.getElementsByClassName("YellowLight")[0].style.display = "block"; break;
+                case 4: added = true; this.lights.push( new Light("OrangeLight", ["#B3672B", "#FD6A02"])); document.getElementsByClassName("OrangeLight")[0].style.display = "block"; break;
+                case 5: added = true; this.lights.push(new Light("SilverLight", ["grey", "#D9DDDC"])); document.getElementsByClassName("SilverLight")[0].style.display = "block"; break;
+                case 6: added = true; this.lights.push(new Light("LightOrange", ["#ffc003", "#ffe6a6"])); document.getElementsByClassName("LightOrange")[0].style.display = "block"; break;
+                case 7: added = true; this.lights.push( new Light("LightBlue", ["#00cee6", "#baf8ff"])); document.getElementsByClassName("LightBlue")[0].style.display = "block"; break;
+                case 8: added = true; this.lights.push(new Light("LightGreen", ["#0fe600", "#c8ffc4"])); document.getElementsByClassName("LightGreen")[0].style.display = "block"; break;
+                case 9: added = true; this.lights.push(new Light("LightRed", ["#ff2626", "#ffc4c4"])); document.getElementsByClassName("LightRed")[0].style.display = "block"; break;
+            }
+            }
+            elem.style.width = (100 / (this.lights.length + 1))+"%";
+            elem.style.height= ((100 / (this.lights.length + 1)) * 3.333) +"%";
+        })
     }
 
     getLightClasses = (classes) => {
@@ -40,21 +66,24 @@ class Controller
                 this.lights[lightID].blink();
             }
             loops++;
-            if(loops === this.blinkOrder.length) {
+            if(loops === this.blinkOrderLength) {
                 window.setTimeout(() => {
                     document.getElementsByClassName("turn")[0].innerHTML = "Its your turn now!";
                     document.getElementsByClassName("turn")[0].style.color = "rgb(175, 255, 175)";
                     console.log(this.blinkOrder)
                     this.record = true;
+                    this.recordCount = 0;
                     window.clearInterval(this.interval)
                 }, 2000)
 
             }
         }, 2000)
     }
-    createRandomInteger = (maxLightCount) => Math.floor(Math.random() * (maxLightCount - 1 + 1 ));
+    createRandomInteger = (maxLightCount) => {
+        console.log(this.lights.length)
+        return Math.floor(Math.random() * (this.lights.length )
+        )} 
 
     toString = () => JSON.parse(JSON.stringify(this))
 }
-
 export let controller = new Controller(1000);
--- src/js/index.js
+++ src/js/index.js
@@ -7,37 +7,94 @@ let RedLight = new Light("RedLight", ["rgb(102, 0, 0)", "rgb(204, 51, 51)"]);
 let BlueLight = new Light("BlueLight", ["rgb(0, 0, 102)", "rgb(51, 51, 204)"]);
 
 Controller.getLightClasses([GreenLight, RedLight, BlueLight]);
-Controller.createBlinkOrder(document.getElementsByClassName("light").length);
 
 window.onClick = (light, id) => {
-    if(Controller.record) {
-        let lights = [GreenLight, RedLight, BlueLight];
-        if(light === window.event.target.id) {
+    if (Controller.record) {
+        let lights = Controller.lights;
+        if (light === window.event.target.id) {
             console.log(lights[id])
             lights[id].blink();
-            
-            if(id === Controller.blinkOrder[Controller.recordCount]) {
+
+            if (id === Controller.blinkOrder[Controller.recordCount]) {
                 console.log("correct")
-            } else {
-                console.error("false")
-            }
-            if(Controller.recordCount === (Controller.blinkOrder.length - 1) ) {
+            } 
+            else {
+                console.error("false");
                 Controller.record = false;
+                document.getElementsByClassName("turn")[0].innerHTML = "You have failed your turn!";
+                document.getElementsByClassName("turn")[0].style.color = "red";
+                document.getElementsByClassName("health__bar" + Controller.hp)[0].style.background = "red";
                 window.setTimeout(() => {
-                    document.getElementsByClassName("turn")[0].innerHTML = "Its not your turn!";
-                    document.getElementsByClassName("turn")[0].style.color = "orange";
+                    document.getElementsByClassName("health__bar" + Controller.hp)[0].style.display = "none";
+                    Controller.hp--;
+                    document.getElementsByClassName("turn")[0].innerHTML = "Replaying same level!";
+                    document.getElementsByClassName("turn")[0].style.color = "red";
+                    Controller.recordCount = 0;
+
+                    if (Controller.hp === 0) {
+                        document.querySelector("main").innerHTML = "<h2> Game-Over </h2>";
+                        document.querySelector("main").style.color = "red";
+                        document.querySelector(".healthbar").style.display = "none";
+                        document.querySelector(".turn").style.display = "none";
+                        document.querySelector(".header__level").style.display = "none";
+                    }
+
+                    window.setTimeout(() => {
+                        document.getElementsByClassName("turn")[0].innerHTML = "Its not your turn!";
+                        document.getElementsByClassName("turn")[0].style.color = "orange";
+                        blink(false);
+                        return;
+                    }, 2000)
                 }, 2000)
             }
-            console.log(Controller.recordCount)
+            if (Controller.recordCount === (Controller.blinkOrder.length - 1)) {
+                Controller.record = false;
+                window.setTimeout(() => {
+                    document.getElementsByClassName("turn")[0].innerHTML = "You guesses were correct!";
+                    document.getElementsByClassName("turn")[0].style.color = "lightgreen";
+                    window.setTimeout(() => {
+                        document.getElementsByClassName("header__level")[0].innerHTML = "lvl-" + (Controller.blinkOrderLength - 1).toString();;
+                        document.getElementsByClassName("turn")[0].innerHTML = "Moving to next level!";
+                        document.getElementsByClassName("turn")[0].style.color = "red";
+                        Controller.recordCount = 0;
+                        Controller.blinkOrderLength = Controller.blinkOrderLength + 1;
+                        if ((Controller.blinkOrderLength + 1) % 4 === 0) {
+                            Controller.addLight();
+                            if (Controller.hp < 3) {
+                                Controller.hp++;
+                                document.getElementsByClassName("health__bar" + Controller.hp)[0].style.display = "block";
+                            }
+                        }
+                        window.setTimeout(() => {
+                            document.getElementsByClassName("turn")[0].innerHTML = "Its not your turn!";
+                            document.getElementsByClassName("turn")[0].style.color = "orange";
+                            blink(true);
+                            return;
+                        }, 2000)
+                    }, 2000)
+                }, 2000)
+            }
             Controller.recordCount++;
         }
     }
 }
 
 window.start = () => {
-    Controller.blinkLights();
+    blink(true);
     document.getElementsByClassName("turn")[0].style.display = "block";
-    document.querySelector("main").style.display = "block";
+    document.querySelector("main").style.display = "flex";
     document.querySelector("button").style.display = "none";
     document.querySelector(".healthbar").style.display = "flex";
+    document.querySelector(".header__level").style.display = "block";
+}
+
+window.blink = (newOrder) => {
+    if (newOrder) {
+        Controller.createBlinkOrder(Controller.blinkOrderLength);
+    }
+    Controller.blinkLights();
+}
+
+window.addLight = () => {
+    Controller.addLight();
 }
